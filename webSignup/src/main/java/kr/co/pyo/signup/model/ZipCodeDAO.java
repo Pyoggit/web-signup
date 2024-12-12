990d7f50c99cabf6fe5def7a5cb1697150b66b11
jangpyo <wkdvy122851@gmail.com>
2024-12-12 12:48:37 +0900

    [CREATE] 주소정보 데이터 임포트

---
+++ webSignup/src/main/java/kr/co/pyo/signup/model/ZipCodeDAO.java
@@ -0,0 +1,46 @@
+package kr.co.pyo.signup.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import kr.co.pyo.signup.util.DBUtility;
+
+public class ZipCodeDAO {
+
+	private final String SELECT_BY_ID_SQL = "SELECT count(*) as count FROM STUDENT WHERE ID = ?";
+	private final String SELECT_ZIP_SQL = "SELECT * FROM ZIPCODE WHERE DONG LIKE ?";
+
+
+	public ArrayList<ZipCodeVO> selectZipCode(ZipCodeVO zvo) {
+		Connection con = DBUtility.dbCon();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<ZipCodeVO> zipList = new ArrayList<ZipCodeVO>();
+		try {
+			pstmt = con.prepareStatement(SELECT_ZIP_SQL);
+			// "박촌동%"
+			String dongValue = "%" + zvo.getDong() + "%";
+			pstmt.setString(1, dongValue);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				String zipcode = rs.getString("zipcode");
+				String sido = rs.getString("sido");
+				String gugun = rs.getString("gugun");
+				String dong = rs.getString("dong");
+				String bunji = rs.getString("bunji");
+				ZipCodeVO obj = new ZipCodeVO(zipcode, sido, gugun, dong, bunji);
+				zipList.add(obj);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtility.dbClose(con, pstmt, rs);
+		}
+		return zipList;
+	}
+	
+
+}
