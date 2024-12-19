e2d1f90bc6f1c56558b357976b0099a4e8e6c17b
jangpyo <wkdvy122851@gmail.com>
2024-12-19 17:38:29 +0900

    [UPDATE] 문의하기 게시판 (Q&A)답변 형식으로 수정

--- webSignup/src/main/java/kr/co/pyo/board/model/BoardDAO.java
+++ webSignup/src/main/java/kr/co/pyo/board/model/BoardDAO.java
@@ -9,110 +9,50 @@ import java.util.ArrayList;
 
 import kr.co.pyo.signup.util.ConnectionPool;
 
+
 public class BoardDAO {
-	// 싱글톤--------------------------------------------------------------------------------------
+	// 싱글톤1
 	private static BoardDAO instance;
 
+	// 싱글톤2
 	private BoardDAO() {
 	}
 
+	// 싱글톤3
 	public static BoardDAO getInstance() {
 		if (instance == null) {
 			synchronized (BoardDAO.class) {
 				instance = new BoardDAO();
 			}
 		}
-
 		return instance;
 	}
 
-	// ------------------------------------------------------------------------------------------
 	private final String SELECT_SQL = "select * from board order by num desc";
 	private final String SELECT_START_END_SQL = "select * from "
 			+ "(select rownum AS rnum, num, writer, email, subject, pass, regdate, readcount, ref, step, depth, content, ip "
-			+ "from (select * from board order by ref desc, step asc)) "
-			+ "where rnum >= ? and rnum <= ?";
-	private final String SELECT_PASS_SQL = "SELECT COUNT(*) AS COUNT FROM BOARD WHERE NUM = ? AND PASS = ? ";
-	private final String SELECT_ONE_SQL = "select * from board where num = ?";
+			+ "from (select * from board order by ref desc, step asc)) where rnum >= ? and rnum <= ?";
 	private final String SELECT_COUNT_SQL = "SELECT COUNT(*) AS COUNT FROM BOARD";
 	private final String SELECT_MAX_NUM_SQL = "select max(num) as NUM from board";
-	private final String INSERT_SQL = "INSERT INTO BOARD (num, writer, email, subject, pass,regdate, ref, step, depth, content, ip)values(board_seq.nextval,?,?,?,?,?,?,?,?,?,?)";
-	private final String DELETE_SQL = "DELETE FROM BOARD WHERE NUM = ? AND PASS = ?";
-	private final String UPDATE_SQL = "update board set writer=?,email=?,subject=?,content=? where num=? ";
-	private final String UPDATE_STEP_SQL = "update board set step=step+1 where ref= ? and step> ?";
-	private final String UPDATE_READCOUNT_SQL = "update board set readcount=readcount+1 where num = ?";
+	private final String SELECT_ONE_SQL = "select * from board where num = ?";
+	private final String SELECT_PASS_ID_CHECK_SQL = "select count(*) count from board where num = ? and pass = ?";
 
-	
-	
+	private final String SELECT_BY_ID_SQL = "SELECT count(*) as count FROM STUDENT WHERE ID = ?";
+	private final String SELECT_LOGIN_SQL = "SELECT PASS FROM STUDENT WHERE ID = ?";
+	private final String INSERT_SQL = "insert into board(num, writer, email, subject, pass,	"
+			+ "regdate, ref, step, depth, content, ip)	values(board_seq.nextval,?,?,?,?,?,?,?,?,?,?)";
+	private final String DELETE_SQL = "DELETE FROM BOARD WHERE NUM = ? and PASS = ?";
+	private final String UPDATE_SQL = "update board set writer= ?,email= ?,subject= ? ,content= ? where num = ?";
+	private final String UPDATE_STEP_SQL = "update board set step=step+1 where ref= ? and step > ?";
+	private final String UPDATE_READCOUNT_SQL = "update board set readcount = readcount+1 where num = ?";
+	private final String SELECT_ZIP_SQL = "select * from zipcode where dong like ?";
 
-	// 게시판 최근 N개 글 가져오기
-	public ArrayList<BoardVO> selectRecentBoards(int limit) {
-	    ArrayList<BoardVO> boardList = new ArrayList<>();
-	    String sql = "SELECT * FROM BOARD ORDER BY REGDATE DESC FETCH FIRST ? ROWS ONLY";
-	    Connection con = null;
-	    PreparedStatement pstmt = null;
-	    ResultSet rs = null;
-
-	    try {
-	        con = ConnectionPool.getInstance().dbCon();
-	        pstmt = con.prepareStatement(sql);
-	        pstmt.setInt(1, limit);
-	        rs = pstmt.executeQuery();
-
-	        while (rs.next()) {
-	        	boardList.add(mapResultSetToBoardVO(rs));
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    } finally {
-	        // 자원 반환
-	        ConnectionPool.getInstance().dbClose(con, pstmt, rs);
-	    }
-	    return boardList;
-	}
-
-	// ResultSet을 BoardVO로 매핑하는 메서드
-	private BoardVO mapResultSetToBoardVO(ResultSet rs) throws SQLException {
-	    BoardVO vo = new BoardVO();
-	    vo.setNum(rs.getInt("NUM"));
-	    vo.setWriter(rs.getString("WRITER"));
-	    vo.setEmail(rs.getString("EMAIL"));
-	    vo.setSubject(rs.getString("SUBJECT"));
-	    vo.setRegdate(rs.getTimestamp("REGDATE"));
-	    vo.setReadcount(rs.getInt("READCOUNT"));
-	    return vo;
-	}
-
-
-	
-	
-	// 게시글 삭제하기
-	public boolean deleteDB(BoardVO vo) {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.dbCon();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        int count = 0; 
-
-        try {
-            pstmt = con.prepareStatement(DELETE_SQL);
-            pstmt.setInt(1, vo.getNum());
-            pstmt.setString(2, vo.getPass());
-            count = pstmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            cp.dbClose(con, pstmt);
-        }
-
-        return (count != 0) ? (true):(false);
-    }
-	
-	// 게시글 입력하기
 	public Boolean insertDB(BoardVO vo) {
 		ConnectionPool cp = ConnectionPool.getInstance();
 		Connection con = cp.dbCon();
 		PreparedStatement pstmt = null;
+
+		// 현재 보드속에 가장최고값 + 1, 없으면 1
 		ResultSet rs = null;
 		int number = 0;
 		int step = 0;
@@ -124,16 +64,15 @@ public class BoardDAO {
 			pstmt = con.prepareStatement(SELECT_MAX_NUM_SQL);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				// 가장최고값 + 1
 				number = rs.getInt("NUM") + 1;
 			} else {
-				// 가장최고값이 없으면 1
 				number = 1;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
+		// getNum() 0 이면 새글, 0 이면 답변글이다.
 		try {
 			if (vo.getNum() != 0) {// 답변글일경우
 				pstmt = con.prepareStatement(UPDATE_STEP_SQL);
@@ -151,10 +90,10 @@ public class BoardDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		// 게시판 글 입력하기
+
+		// 게시판 글 등록하기
 		try {
 			pstmt = con.prepareStatement(INSERT_SQL);
-
 			pstmt.setString(1, vo.getWriter());
 			pstmt.setString(2, vo.getEmail());
 			pstmt.setString(3, vo.getSubject());
@@ -165,17 +104,15 @@ public class BoardDAO {
 			pstmt.setInt(8, depth);
 			pstmt.setString(9, vo.getContent());
 			pstmt.setString(10, vo.getIp());
-
 			count = pstmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			cp.dbClose(con, pstmt);
 		}
-		return (count > 0) ? (true) : (false);
+		return (count > 0) ? true : false;
 	}
 
-	// 게시글 숫자 검색하기
 	public int selectCountDB() {
 		ConnectionPool cp = ConnectionPool.getInstance();
 		Connection con = cp.dbCon();
@@ -196,7 +133,40 @@ public class BoardDAO {
 		return count;
 	}
 
-	// 페이징 기법
+	public ArrayList<BoardVO> selectDB() {
+		ConnectionPool cp = ConnectionPool.getInstance();
+		Connection con = cp.dbCon();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<BoardVO> boardList = new ArrayList<BoardVO>();
+		try {
+			pstmt = con.prepareStatement(SELECT_SQL);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				int num = rs.getInt("num");
+				String writer = rs.getString("writer");
+				String email = rs.getString("email");
+				String subject = rs.getString("subject");
+				String pass = rs.getString("pass");
+				Timestamp regdate = rs.getTimestamp("regdate");
+				int readcount = rs.getInt("readcount");
+				int ref = rs.getInt("ref");
+				int step = rs.getInt("step");
+				int depth = rs.getInt("depth");
+				String content = rs.getString("content");
+				String ip = rs.getString("ip");
+				BoardVO vo = new BoardVO(num, writer, email, subject, pass, readcount, ref, step, depth, regdate,
+						content, ip);
+				boardList.add(vo);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			cp.dbClose(con, pstmt, rs);
+		}
+		return boardList;
+	}
+	
 	public ArrayList<BoardVO> selectStartEndDB(int start, int end) {
 		ConnectionPool cp = ConnectionPool.getInstance();
 		Connection con = cp.dbCon();
@@ -233,54 +203,21 @@ public class BoardDAO {
 		return boardList;
 	}
 
-	//게시글 전체 검색하기
-	public ArrayList<BoardVO> selectDB() {
-		ConnectionPool cp = ConnectionPool.getInstance();
-		Connection con = cp.dbCon();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		ArrayList<BoardVO> boardList = new ArrayList<BoardVO>();
-		try {
-			pstmt = con.prepareStatement(SELECT_SQL);
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				int num = rs.getInt("num");
-				String writer = rs.getString("writer");
-				String email = rs.getString("email");
-				String subject = rs.getString("subject");
-				String pass = rs.getString("pass");
-				Timestamp regdate = rs.getTimestamp("regdate");
-				int readcount = rs.getInt("readcount");
-				int ref = rs.getInt("ref");
-				int step = rs.getInt("step");
-				int depth = rs.getInt("depth");
-				String content = rs.getString("content");
-				String ip = rs.getString("ip");
-				BoardVO vo = new BoardVO(num, writer, email, subject, pass, readcount, ref, step, depth, regdate,
-						content, ip);
-				boardList.add(vo);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			cp.dbClose(con, pstmt, rs);
-		}
-		return boardList;
-	}
-	
-	// 게시글 번호검색해서 조회수 조회
+
 	public BoardVO selectBoardDB(BoardVO vo) {
 		ConnectionPool cp = ConnectionPool.getInstance();
 		Connection con = cp.dbCon();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		BoardVO bvo = null;
-
+		int count = 0;
 		try {
+			// 조회수 증가
 			pstmt = con.prepareStatement(UPDATE_READCOUNT_SQL);
 			pstmt.setInt(1, vo.getNum());
 			pstmt.executeUpdate();
 
+			// num 내용가져오기
 			pstmt = con.prepareStatement(SELECT_ONE_SQL);
 			pstmt.setInt(1, vo.getNum());
 			rs = pstmt.executeQuery();
@@ -304,17 +241,17 @@ public class BoardDAO {
 		} finally {
 			cp.dbClose(con, pstmt, rs);
 		}
+		System.out.println("bvo = " + bvo.toString());
 		return bvo;
 	}
 
-	// 게시글 번호 검색
 	public BoardVO selectBoardOneDB(BoardVO vo) {
 		ConnectionPool cp = ConnectionPool.getInstance();
 		Connection con = cp.dbCon();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		BoardVO bvo = null;
-
+		int count = 0;
 		try {
 			pstmt = con.prepareStatement(SELECT_ONE_SQL);
 			pstmt.setInt(1, vo.getNum());
@@ -339,11 +276,12 @@ public class BoardDAO {
 		} finally {
 			cp.dbClose(con, pstmt, rs);
 		}
+		System.out.println("bvo = " + bvo.toString());
 		return bvo;
 	}
 
-	// 게시글 수정하기
 	public int updateDB(BoardVO vo) {
+		// 1: 성공, 2. 패스워드문제, 3 수정문제
 		ConnectionPool cp = ConnectionPool.getInstance();
 		Connection con = cp.dbCon();
 		PreparedStatement pstmt = null;
@@ -352,8 +290,9 @@ public class BoardDAO {
 		int count = 0;
 		int returnValue = 1;
 
+		// 패스워드가 맞는지 점검필요
 		try {
-			pstmt = con.prepareStatement(SELECT_PASS_SQL);
+			pstmt = con.prepareStatement(SELECT_PASS_ID_CHECK_SQL);
 			pstmt.setInt(1, vo.getNum());
 			pstmt.setString(2, vo.getPass());
 			rs = pstmt.executeQuery();
@@ -361,6 +300,7 @@ public class BoardDAO {
 				passCheckCount = rs.getInt("COUNT");
 			}
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -373,21 +313,38 @@ public class BoardDAO {
 				pstmt.setString(4, vo.getContent());
 				pstmt.setInt(5, vo.getNum());
 				count = pstmt.executeUpdate();
-				if (count == 0) {
+				if (count == 0)
 					returnValue = 3;
-				}else {
+				else
 					returnValue = 1;
-				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			} finally {
 				cp.dbClose(con, pstmt);
 			}
-
-		}else {
+		} else {
 			returnValue = 2;
 		}
 		return returnValue;
 	}
 
+	public boolean deleteDB(BoardVO vo) {
+		ConnectionPool cp = ConnectionPool.getInstance();
+		Connection con = cp.dbCon();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int count = 0; 
+		try {
+			pstmt = con.prepareStatement(DELETE_SQL);
+			pstmt.setInt(1, vo.getNum());
+			pstmt.setString(2, vo.getPass());
+			count = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			cp.dbClose(con, pstmt);
+		}
+
+		return (count != 0) ? true: false;
+	}
 }
