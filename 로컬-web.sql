d882910f739bc13e8c9e25102af3a4c1637b34ab
jangpyo <wkdvy122851@gmail.com>
2024-12-23 11:09:50 +0900

    [UPDATE] 도서 상품 테이블  SQL 쿼리문 추가

--- 로컬-web.sql
+++ 로컬-web.sql
@@ -1,15 +1,14 @@
 -- 회원가입 페이지 구현
 
 -- 회원가입 테이블 생성
-DROP TABLE SIGNUP;
 CREATE TABLE SIGNUP (
     ID VARCHAR2(12) ,                         -- 아이디 (4~12자의 영문 대소문자와 숫자)
     PWD VARCHAR2(12) NOT NULL,                -- 비밀번호 (4~12자의 영문 대소문자와 숫자)
     NAME VARCHAR2(50) NOT NULL,               -- 이름
     BIRTH VARCHAR2(10),                       -- 생년월일 (20240101)
-    PHONE1 VARCHAR2(3) NOT NULL,              -- 전화번호 
-    PHONE2 VARCHAR2(4) NOT NULL,
-    PHONE3 VARCHAR2(4) NOT NULL,
+    PHONE1 VARCHAR2(3) NOT NULL,              -- 전화번호 (첫번째)
+    PHONE2 VARCHAR2(4) NOT NULL,              -- 전화번호 (중간)
+    PHONE3 VARCHAR2(4) NOT NULL,              -- 전화번호 (마지막)
     EMAIL VARCHAR2(30) NOT NULL,              -- 이메일 주소
     ZIPCODE VARCHAR2(7) NOT NULL,             -- 우편번호
     ADDRESS1 VARCHAR2(120) NOT NULL,          -- 주소
@@ -17,8 +16,6 @@ CREATE TABLE SIGNUP (
 );
 
 ALTER TABLE SIGNUP ADD CONSTRAINT SIGNUP_ID_PK PRIMARY KEY(ID);
-
-SELECT BIRTH FROM SIGNUP WHERE ID = 'aaa123';
 
 -- 주소정보테이블
 CREATE TABLE ZIPCODE (
@@ -34,12 +31,7 @@ ALTER TABLE ZIPCODE ADD CONSTRAINT ZIPCODE_SEQ_PK PRIMARY KEY(SEQ);
 
 SELECT * FROM ZIPCODE;
 
-SELECT * FROM ZIPCODE WHERE DONG LIKE '권선%';
-
-SELECT PHONE1, PHONE2, PHONE3 FROM SIGNUP WHERE ID = 'aaa123';
-
 -- 게시판 테이블
-DROP TABLE BOARD;
 CREATE TABLE BOARD (
     NUM         NUMBER(7,0) NOT NULL, 
     WRITER      VARCHAR2(12) NOT NULL,   
@@ -57,11 +49,6 @@ CREATE TABLE BOARD (
 
 ALTER TABLE BOARD ADD CONSTRAINT BOARD_NUM_PK PRIMARY KEY(NUM);
 
-select * from board where num = 1;
-
-update board set readcount=readcount+1 where num = 1;
-commit;
-
 SELECT * FROM BOARD;
 
 CREATE SEQUENCE BOARD_SEQ   
@@ -70,8 +57,6 @@ CREATE SEQUENCE BOARD_SEQ
  NOMAXVALUE                 -- 최대값이 무한대..
  NOCACHE
  NOCYCLE;
- 
-SELECT * FROM BOARD;
 
 SELECT COUNT(*) AS COUNT FROM BOARD;
  
@@ -94,11 +79,6 @@ CREATE TABLE NOTICE (
 );
 
 ALTER TABLE NOTICE ADD CONSTRAINT NOTICE_NUM_PK PRIMARY KEY(NUM);
-
-SELECT * FROM NOTICE WHERE NUM = 1;
-
-UPDATE NOTICE SET READCOUNT = READCOUNT + 1 WHERE NUM = 1;
-COMMIT;
 
 SELECT * FROM NOTICE;
 
@@ -130,7 +110,6 @@ CREATE TABLE FREEBOARD (
     IP          VARCHAR2(20) NOT NULL
 );
 
--- 기본키 제약조건 추가
 ALTER TABLE FREEBOARD ADD CONSTRAINT FREEBOARD_NUM_PK PRIMARY KEY(NUM);
 
 -- 자유게시판 시퀀스 생성
@@ -140,28 +119,21 @@ CREATE SEQUENCE FREEBOARD_SEQ
  NOMAXVALUE                  -- 최대값이 무한대
  NOCACHE
  NOCYCLE;
-
--- 데이터 조회 예시
-SELECT * FROM FREEBOARD WHERE NUM = 1;
-
--- 조회수 증가 예시
-UPDATE FREEBOARD SET READCOUNT = READCOUNT + 1 WHERE NUM = 1;
-COMMIT;
-
--- 전체 데이터 조회
+ 
 SELECT * FROM FREEBOARD;
 
--- 전체 개수 확인
 SELECT COUNT(*) AS COUNT FROM FREEBOARD;
 
+-- 자유게시판 댓글작성 테이블
 CREATE TABLE FREEBOARDCOMMENT (
-    COMMENTID   NUMBER(10) PRIMARY KEY,   -- 댓글 ID
-    BOARDNUM    NUMBER(10),               -- 게시글 번호 (외래 키)
-    WRITER       VARCHAR2(50),             -- 댓글 작성자
-    CONTENT      VARCHAR2(1000),           -- 댓글 내용
-    REGDATE      TIMESTAMP DEFAULT SYSDATE -- 댓글 작성 시간
+    COMMENTID   NUMBER(10) PRIMARY KEY,  
+    BOARDNUM    NUMBER(10),               
+    WRITER       VARCHAR2(50),           
+    CONTENT      VARCHAR2(1000),           
+    REGDATE      TIMESTAMP DEFAULT SYSDATE 
 );
 
+-- 자유게시판의 게시판 번호 외래키 생성
 ALTER TABLE FREEBOARDCOMMENT 
 ADD CONSTRAINT FK_BOARD_COMMENT FOREIGN KEY (BOARDNUM) REFERENCES FREEBOARD(NUM);
 
@@ -172,6 +144,8 @@ INCREMENT BY 1
 NOMAXVALUE
 NOCYCLE;
 
+
+-- 도서상품 테이블
 CREATE TABLE Product (
     bookID VARCHAR2(50) PRIMARY KEY, -- 도서 ID
     bookName VARCHAR2(100) NOT NULL, -- 도서명
@@ -185,10 +159,13 @@ CREATE TABLE Product (
     productCondition VARCHAR2(10) CHECK (productCondition IN ('New', 'Old', 'E-Book')) -- 상태 제한
 );
 
+-- 장바구니 컬럼 추가
+ALTER TABLE Product ADD quantity NUMBER(10) DEFAULT 0;
+
+
 INSERT INTO Product (bookID, bookName, unitPrice, description, author, publisher, category, filename, unitsInStock, productCondition) 
 VALUES ('P001', 'Sample Book', 10000, 'This is a sample book description.', 'John Doe', 'Sample Publisher', 'Fiction', 'sample.jpg', 100, 'New');
 commit;
 
 SELECT * FROM Product;
-
 
